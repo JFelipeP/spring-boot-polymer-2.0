d6d502a58edbfca314d5ac2533eac62630bafb27
jsflppereira <jsflppereira@gmail.com>
2017-07-04 19:09:52 -0300

    improved data visualization

--- src/main/java/org/polymer/controller/CarController.java
+++ src/main/java/org/polymer/controller/CarController.java
@@ -5,19 +5,29 @@ import java.util.List;
 import org.polymer.model.Car;
 import org.polymer.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/api")
-@RestController
+@Controller
 public class CarController {
 	
 	@Autowired private CarRepository carRepository;	
 	
 	@GetMapping("/cars")
+	@ResponseBody
 	public List<Car> getCars() {
 		return carRepository.findAll();
+	}
+	
+	@PostMapping(value="/save", consumes={"application/json"})
+	@ResponseBody
+	public Car save(@RequestBody Car car) {		
+		return carRepository.save(car);
 	}
 
 }
--- src/main/java/org/polymer/model/Car.java
+++ src/main/java/org/polymer/model/Car.java
@@ -1,12 +1,15 @@
 package org.polymer.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Car {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String make;
 	private String model;
@@ -50,6 +53,13 @@ public class Car {
 	public void setSpeed(String speed) {
 		this.speed = speed;
 	}
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", make=" + make + ", model=" + model + ", year=" + year + ", power=" + power
+				+ ", speed=" + speed + "]";
+	}
+	
+	
 	
 	
 
