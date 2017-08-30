472310d4ed5f8bcb1c44453244bdd200fcf650b6
jsflppereira <jsflppereira@gmail.com>
2017-08-30 13:10:36 -0300

    updates sql data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,4 @@
 insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
-insert into car (make, model, year, power, speed) values ('Ferrari', 'LaFerrari', '2017', '949 hp', '350 km/h');
+insert into car (make, model, year, power, speed) values ('Mazda', 'MX-5 4th G.', '2017', '158 hp', '215 km/h');
+insert into car (make, model, year, power, speed) values ('Lamborghini', 'Centenario', '2017', '759 hp', '350 km/h');
+
